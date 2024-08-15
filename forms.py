f2f0f31b21a8a587fe16ee3e654ca3dc1938ea9e
ksg-dev <ksg.dev.data@gmail.com>
2024-08-15 15:13:58 -0500

    create register form

--- forms.py
+++ forms.py
@@ -1,20 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import InputRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
 # WTForm for creating a blog post
 class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    title = StringField("Blog Post Title", validators=[InputRequired()])
+    subtitle = StringField("Subtitle", validators=[InputRequired()])
+    img_url = StringField("Blog Image URL", validators=[InputRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[InputRequired()])
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
-
+# Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email",
+                        validators=[
+                            InputRequired(),
+                            Email()
+                        ]
+                        )
+    name = StringField("Name",
+                       validators=[
+                           InputRequired()
+                       ]
+                       )
+    password = StringField("Password",
+                           validators=[
+                               InputRequired()
+                           ]
+                           )
+    submit = SubmitField("Sign Up")
 
 # TODO: Create a LoginForm to login existing users
 
